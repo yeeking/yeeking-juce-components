6bf7401cc0bed09b8fabb0505dfdb37479062740
Matthew Yee-King on yogurt <m.yee-king@gold.ac.uk>
2020-05-19 18:39:34 +0100

    updated the clock

--- JuceSequencer/Source/Sequencer.h
+++ JuceSequencer/Source/Sequencer.h
@@ -10,15 +10,18 @@
 #include <string>
 #include <thread>
 #include <chrono>
-
+#include <functional>
 
 class SimpleClock 
 {
 
   public:
-    SimpleClock() : sleepTimeMs{5}, running{false}
-    {}
-
+    SimpleClock() : sleepTimeMs{5}, running{false}, callback{
+      [](){
+          std::cout << "SimpleClock::default tick callback" << std::endl;
+      }
+     } 
+     {}
     ~SimpleClock()
     {
       stop();
@@ -42,10 +45,15 @@ class SimpleClock
         delete tickThread;
       }
     }
+    /** set the function to be called when the click ticks */
+    void setCallback(std::function<void()> c){
+      callback = c;
+    }
     void tick()
     {
       // call the callback
-      std::cout << "SimpleClock::tick" << std::endl;      
+      std::cout << "SimpleClock::tick" << std::endl; 
+      callback();     
     }
 
  static void ticker(SimpleClock* clock, long intervalMs, long sleepTimeMs)
@@ -69,30 +77,23 @@ class SimpleClock
           // sleep - but how much?
           remainingMs = intervalMs - elapsedMs;
           if (remainingMs > sleepTimeMs) // sleep as much as possible
-          {
             std::this_thread::sleep_for(std::chrono::milliseconds(remainingMs - sleepTimeMs));
-          }
           else
-          {
             std::this_thread::sleep_for(std::chrono::milliseconds(sleepTimeMs));
-          }
-          
         }
       }
     }
     
-      //void setCaallback();
   private:
-
     static long getNow()
     {
       return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
     }
     long intervalMs;
     long sleepTimeMs; // lower means more precision in the timing
-    bool running; 
-    
+    bool running;     
     std::thread* tickThread;
+    std::function<void()> callback;
 };
 
 
@@ -327,12 +328,12 @@ class SequencerEditor {
   {
     return currentSequence;
   }
-
+  /**  */
   int getCurrentStep() const 
   {
     return currentStep;
   }
- 
+  /** which data point in a step are we editing */
   int getCurrentStepIndex() const
   {
     return currentStepIndex;
@@ -348,9 +349,10 @@ class SequencerEditor {
     int currentStepIndex;
     
     SequencerEditorMode editMode;
-    float stepIncrement;
-    
+    float stepIncrement;    
 };
+
+
 
 
 
